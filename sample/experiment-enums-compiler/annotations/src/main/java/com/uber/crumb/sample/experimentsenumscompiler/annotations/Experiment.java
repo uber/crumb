6c4a145a20f8547b231d0c4d3f2a75c52fb65415
Zac Sweers <zac.sweers@gmail.com>
2018-01-31 17:52:48 +0000

    Add experiment annotation

---
+++ sample/experiment-enums-compiler/annotations/src/main/java/com/uber/crumb/sample/experimentsenumscompiler/annotations/Experiment.java
@@ -0,0 +1,11 @@
+package com.uber.crumb.sample.experimentsenumscompiler.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Retention(CLASS)
+@Target(TYPE)
+public @interface Experiment {}
