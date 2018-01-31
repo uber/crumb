6f12e014cbdd37d2b46f71fa88dfbb76d97326ae
Zac Sweers <zac.sweers@gmail.com>
2018-01-31 17:53:00 +0000

    Add dummy experiments usages

---
+++ sample/jvm/java/library/src/main/java/com/uber/crumb/sample/LibraryExperiments.java
@@ -0,0 +1,13 @@
+package com.uber.crumb.sample;
+
+import com.uber.crumb.sample.experimentsenumscompiler.annotations.Experiment;
+
+@Experiment
+public enum LibraryExperiments {
+  XP_A,
+  XP_C,
+  XP_D,
+  XP_E,
+  XP_F,
+  XP_G,
+}
