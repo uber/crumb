f25e6fb66cb1386e0d8d55293f4ab27a4a85f869
Zac Sweers <zac.sweers@gmail.com>
2018-02-01 10:11:18 +0000

    Add missing qualifier to experiments

--- sample/experiment-enums-compiler/annotations/src/main/java/com/uber/crumb/sample/experimentsenumscompiler/annotations/Experiments.java
+++ sample/experiment-enums-compiler/annotations/src/main/java/com/uber/crumb/sample/experimentsenumscompiler/annotations/Experiments.java
@@ -1,11 +1,13 @@
 package com.uber.crumb.sample.experimentsenumscompiler.annotations;
 
+import com.uber.crumb.annotations.CrumbQualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
+@CrumbQualifier
 @Retention(CLASS)
 @Target(TYPE)
 public @interface Experiments {}
