daf27281a1dd0231c2359eeb72f3f21f90d7aa1e
Zac Sweers <zac.sweers@gmail.com>
2018-03-26 21:42:53 -0700

    Opportunistic doc fixes and run spotlessApply

--- sample/plugins-compiler/plugins-compiler/java/src/main/java/com/uber/crumb/sample/pluginscompiler/package-info.java
+++ sample/plugins-compiler/plugins-compiler/java/src/main/java/com/uber/crumb/sample/pluginscompiler/package-info.java
@@ -13,5 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/** Plugins compiler demo. */
 @com.uber.javaxextras.EverythingIsNonNullByDefault
 package com.uber.crumb.sample.pluginscompiler;
