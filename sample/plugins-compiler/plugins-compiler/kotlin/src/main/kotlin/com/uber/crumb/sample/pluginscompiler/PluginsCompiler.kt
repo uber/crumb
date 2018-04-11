beb03288fae506a5dd2d1fe417008ea6fd914bc6
Zac Sweers <zac.sweers@gmail.com>
2018-04-11 03:27:49 -0700

    Add local annotation parser

--- sample/plugins-compiler/plugins-compiler/kotlin/src/main/kotlin/com/uber/crumb/sample/pluginscompiler/PluginsCompiler.kt
+++ sample/plugins-compiler/plugins-compiler/kotlin/src/main/kotlin/com/uber/crumb/sample/pluginscompiler/PluginsCompiler.kt
@@ -35,9 +35,19 @@ import me.eugeniomarletti.kotlin.metadata.KotlinClassMetadata
 import me.eugeniomarletti.kotlin.metadata.classKind
 import me.eugeniomarletti.kotlin.metadata.kaptGeneratedOption
 import me.eugeniomarletti.kotlin.metadata.kotlinMetadata
+import me.eugeniomarletti.kotlin.metadata.kotlinMetadataAnnotation
+import org.jetbrains.kotlin.load.java.JvmAnnotationNames
+import org.jetbrains.kotlin.load.java.JvmBytecodeBinaryVersion
+import org.jetbrains.kotlin.load.kotlin.JvmMetadataVersion
+import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader
+import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader.Kind.FILE_FACADE
+import org.jetbrains.kotlin.load.kotlin.header.KotlinClassHeader.Kind.MULTIFILE_CLASS_PART
 import org.jetbrains.kotlin.serialization.ProtoBuf.Class.Kind
 import java.io.File
+import javax.annotation.processing.Messager
 import javax.lang.model.element.AnnotationMirror
+import javax.lang.model.element.AnnotationValue
+import javax.lang.model.element.Element
 import javax.lang.model.element.ElementKind
 import javax.lang.model.element.ElementKind.CLASS
 import javax.lang.model.element.Modifier
@@ -88,13 +98,14 @@ class PluginsCompiler : CrumbProducerExtension, CrumbConsumerExtension {
       return
     }
 
+    val classHeader = type.kotlinClassHeader2(messager = context.processingEnv.messager)
     val kmetadata = type.kotlinMetadata
 
     if (kmetadata !is KotlinClassMetadata) {
       context.processingEnv
           .messager
           .printMessage(ERROR,
-              "@${PluginPoint::class.java.simpleName} can't be applied to $type: must be a class. KMetadata was $kmetadata and annotations were [${type.annotationMirrors.joinToString { it.annotationType.asElement().simpleName }}]",
+              "@${PluginPoint::class.java.simpleName} can't be applied to $type: must be a class. KMetadata was $kmetadata and classheader was $classHeader and annotations were [${type.annotationMirrors.joinToString { it.annotationType.asElement().toString() }}]",
               type)
       return
     }
@@ -206,3 +217,78 @@ class PluginsCompiler : CrumbProducerExtension, CrumbConsumerExtension {
 
   override fun key() = METADATA_KEY
 }
+
+fun Element.kotlinClassHeader2(messager: Messager): KotlinClassHeader? {
+  var metadataVersion: JvmMetadataVersion? = null
+  var bytecodeVersion: JvmBytecodeBinaryVersion? = null
+  var extraString: String? = null
+  var extraInt = 0
+  var data: Array<String>? = null
+  var strings: Array<String>? = null
+  var incompatibleData: Array<String>? = null
+  var headerKind: KotlinClassHeader.Kind? = null
+  var packageName: String? = null
+
+  for (annotation in annotationMirrors) {
+    if ((annotation.annotationType.asElement() as TypeElement).qualifiedName.toString() != kotlinMetadataAnnotation) continue
+    messager.printMessage(Diagnostic.Kind.WARNING, "Found a metadata! ${annotation.getElementValues()}")
+
+    for ((element, _value) in annotation.elementValues) {
+      messager.printMessage(Diagnostic.Kind.WARNING, "Reading values: $element and $_value")
+      val name = element.simpleName.toString().takeIf { it.isNotEmpty() } ?: continue
+      val value: Any? = unwrapAnnotationValue(_value)
+      when {
+        JvmAnnotationNames.KIND_FIELD_NAME == name && value is Int ->
+          headerKind = KotlinClassHeader.Kind.getById(value)
+        JvmAnnotationNames.METADATA_VERSION_FIELD_NAME == name ->
+          metadataVersion = JvmMetadataVersion(*@Suppress("UNCHECKED_CAST") (value as List<Int>).toIntArray())
+        JvmAnnotationNames.BYTECODE_VERSION_FIELD_NAME == name ->
+          bytecodeVersion = JvmBytecodeBinaryVersion(*@Suppress("UNCHECKED_CAST") (value as List<Int>).toIntArray())
+        JvmAnnotationNames.METADATA_EXTRA_STRING_FIELD_NAME == name && value is String ->
+          extraString = value
+        JvmAnnotationNames.METADATA_EXTRA_INT_FIELD_NAME == name && value is Int ->
+          extraInt = value
+        JvmAnnotationNames.METADATA_DATA_FIELD_NAME == name ->
+          data = @Suppress("UNCHECKED_CAST") (value as List<String>).toTypedArray()
+        JvmAnnotationNames.METADATA_STRINGS_FIELD_NAME == name ->
+          strings = @Suppress("UNCHECKED_CAST") (value as List<String>).toTypedArray()
+        JvmAnnotationNames.METADATA_PACKAGE_NAME_FIELD_NAME == name && value is String ->
+          packageName = value
+      }
+    }
+  }
+  messager.printMessage(Diagnostic.Kind.WARNING, "Done reading elementValues")
+
+  if (headerKind == null) {
+    return null
+  }
+
+  if (metadataVersion == null || !metadataVersion.isCompatible()) {
+    incompatibleData = data
+    data = null
+  }
+  else if ((headerKind == KotlinClassHeader.Kind.CLASS || headerKind == FILE_FACADE || headerKind == MULTIFILE_CLASS_PART) && data == null) {
+    // This means that the annotation is found and its ABI version is compatible, but there's no "data" string array in it.
+    // We tell the outside world that there's really no annotation at all
+    return null
+  }
+
+  return KotlinClassHeader(
+      headerKind,
+      metadataVersion ?: JvmMetadataVersion.INVALID_VERSION,
+      bytecodeVersion ?: JvmBytecodeBinaryVersion.INVALID_VERSION,
+      data,
+      incompatibleData,
+      strings,
+      extraString,
+      extraInt,
+      packageName
+  )
+}
+
+private tailrec fun unwrapAnnotationValue(value: Any?): Any? =
+    when (value) {
+      is AnnotationValue -> unwrapAnnotationValue(value.value)
+      is List<*> -> value.map(::unwrapAnnotationValue)
+      else -> value
+    }
